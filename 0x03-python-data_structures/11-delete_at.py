f474dbab0d2f4b4af3c658354480193f84c28db2
GH770 <mohmed_abdallah770@hotmail.com>
2024-01-08 13:20:20 -0800

    tasks

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,9 +3,7 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
 
-    updated_list = []
-    for i in range(len(my_list)):
-        if i != idx:
-            updated_list.append(my_list[i])
+    updated_list = my_list
+    del my_list[idx]
 
     return updated_list
